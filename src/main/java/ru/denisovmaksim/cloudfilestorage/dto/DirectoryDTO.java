5fbf1b33731f2d3ee14b51b02e663d7db4cde0e7
max <max.nick.demax@gmail.com>
2024-10-21 19:21:08 +0300

    remove FoldersDTO from DirectoryDTO keep one list of StorageObjects

--- src/main/java/ru/denisovmaksim/cloudfilestorage/dto/DirectoryDTO.java
+++ src/main/java/ru/denisovmaksim/cloudfilestorage/dto/DirectoryDTO.java
@@ -1,19 +1,21 @@
 package ru.denisovmaksim.cloudfilestorage.dto;
 
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 import ru.denisovmaksim.cloudfilestorage.model.StorageObject;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Getter
-@Setter
-@NoArgsConstructor
 public class DirectoryDTO {
-    private String path = "";
-    private final List<FolderDTO> upperDirs = new ArrayList<>();
-    private final List<FolderDTO> folders = new ArrayList<>();
-    private final List<StorageObject> files = new ArrayList<>();
+    private final String path;
+    private final List<FolderDTO> upperDirs;
+    //private final List<FolderDTO> folders = new ArrayList<>();
+    private final List<StorageObject> objects;
+
+
+    public DirectoryDTO(String path, List<FolderDTO> upperDirs, List<StorageObject> objects) {
+        this.path = path;
+        this.upperDirs = upperDirs;
+        this.objects = objects;
+    }
 }
