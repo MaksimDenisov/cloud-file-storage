627e82da273c1f7c4410decf83eb0394862af0be
max <max.nick.demax@gmail.com>
2024-12-26 18:24:25 +0300

    refactor: add log Environment Variables
    When using a dev profile, the values of environment variables are logged.

--- src/main/java/ru/denisovmaksim/cloudfilestorage/CloudFileStorageApplication.java
+++ src/main/java/ru/denisovmaksim/cloudfilestorage/CloudFileStorageApplication.java
@@ -1,23 +1,35 @@
 package ru.denisovmaksim.cloudfilestorage;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableTransactionManagement
+@Slf4j
 public class CloudFileStorageApplication {
     public static void main(String[] args) {
         if (System.getenv().getOrDefault("APP_ENV", "dev").equals("dev")) {
-            System.out.printf("JDBC_ROOT      = " + System.getenv().get("JDBC_ROOT") + "\n");
-            System.out.printf("JDBC_PASSWORD  = " + System.getenv().get("JDBC_PASSWORD") + "\n");
-            System.out.printf("JDBC_URL       = " + System.getenv().get("JDBC_URL") + "\n");
-            System.out.printf("REDIS_HOST     = " + System.getenv().get("REDIS_HOST") + "\n");
-            System.out.printf("REDIS_PORT     = " + System.getenv().get("REDIS_PORT") + "\n");
-            System.out.printf("REDIS_PASSWORD = " + System.getenv().get("REDIS_PASSWORD") + "\n");
-            System.out.printf("MINIO_URL      = " + System.getenv().get("MINIO_URL") + "\n");
-            System.out.printf("MINIO_USER     = " + System.getenv().get("MINIO_USER") + "\n");
-            System.out.printf("MINIO_PASSWORD = " + System.getenv().get("MINIO_PASSWORD") + "\n");
+            log.info("Environment Variables:");
+            log.info("JDBC_ROOT      = {}",
+                    System.getenv().getOrDefault("JDBC_ROOT", " Default value"));
+            log.info("JDBC_PASSWORD  = {}",
+                    System.getenv().getOrDefault("JDBC_PASSWORD", " Default value"));
+            log.info("JDBC_URL       = {}",
+                    System.getenv().getOrDefault("JDBC_URL", " Default value"));
+            log.info("REDIS_HOST     = {}",
+                    System.getenv().getOrDefault("REDIS_HOST", " Default value"));
+            log.info("REDIS_PORT     = {}",
+                    System.getenv().getOrDefault("REDIS_PORT", " Default value"));
+            log.info("REDIS_PASSWORD = {}",
+                    System.getenv().getOrDefault("REDIS_PASSWORD", " Default value"));
+            log.info("MINIO_URL      = {}",
+                    System.getenv().getOrDefault("MINIO_URL", " Default value"));
+            log.info("MINIO_USER     = {}",
+                    System.getenv().getOrDefault("MINIO_USER", " Default value"));
+            log.info("MINIO_PASSWORD = {}",
+                    System.getenv().getOrDefault("MINIO_PASSWORD", " Default value"));
         }
         new SpringApplicationBuilder(CloudFileStorageApplication.class)
                 .profiles(System.getenv().getOrDefault("APP_ENV", "dev"))
